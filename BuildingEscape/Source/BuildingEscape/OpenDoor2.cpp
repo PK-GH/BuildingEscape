52d31a069802837e2fd709cc3c971bd8a430489d
PK-GH <pkvac@sfu.ca>
2018-09-22 18:31:47 -0700

    Only Darkness

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor2.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor2.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor2::UOpenDoor2()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UOpenDoor2::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OpeningActor = GetWorld()->GetFirstPlayerController()->GetPawn();
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpenDoor2::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (PressurePlate->IsOverlappingActor(OpeningActor))
+	{
+		OpenDoor2();
+	}
+	// ...
+}
+void UOpenDoor2::OpenDoor2()
+{
+	AActor* Owner = GetOwner();
+	FRotator NewRotation = FRotator(0.0f, 0.0f, 0.0f);
+	Owner->SetActorRotation(NewRotation);
+}
+
---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor2.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "OpenDoor2.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenDoor2 : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor2();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void OpenDoor2();
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.0f;
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+	AActor* OpeningActor;
+};
